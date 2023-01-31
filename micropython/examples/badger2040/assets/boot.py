d4e73b6ab8b5ec8baad71f28856230cd1424e6ed
Mike Szczys <mike@golioth.io>
2023-01-31 14:35:53 -0600

    init will be called during class object creation

--- micropython/examples/badger2040/assets/boot.py
+++ micropython/examples/badger2040/assets/boot.py
@@ -4,7 +4,6 @@ except OSError:
     with open("main.py", "w") as f:
         f.write("from ostentus import ostentus\n\n")
         f.write("o = ostentus()\n")
-        f.write("o.init()\n")
         f.write("o.listen()\n")
         f.flush()
 
