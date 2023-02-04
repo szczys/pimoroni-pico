77245c0ca4d50fc4b9052db06cb785d1da94d06a
Mike Szczys <mike@golioth.io>
2023-02-04 15:31:10 -0600

    use single address and structs for fifo
    This commit moves away from separate addresses for commands and data and
    instead uses a single address for everything. It also groups the fifo
    variables into structs for better readability.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- micropython/modules/ostentus_i2c/i2c_fifo.c
+++ micropython/modules/ostentus_i2c/i2c_fifo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 #include "i2c_multi.h"
 #include "pico/stdlib.h"
 
-#define OSTENTUS_CMD_ADDR 0x12
-#define OSTENTUS_DAT_ADDR 0x13
+#define OSTENTUS_ADDR 0x12
+
 PIO pio = pio0;
 uint pin = 4;
 #define BUFFER_SIZE 64
@@ -12,31 +14,48 @@ uint8_t buffer[BUFFER_SIZE] = {0};
 
 #define FIFO_SIZE 32
 #define FIFO_DATA_OFFSET 2 // cmd addr, data_len
-uint8_t _cur_op = 0;
-uint8_t _fifo[FIFO_SIZE][BUFFER_SIZE];
-uint8_t _has_data;
-uint8_t _cmd_idx;
-uint8_t _dat_idx;
-uint8_t _head;
-uint8_t _tail;
+#define FIFO_DATA_SIZE BUFFER_SIZE-FIFO_DATA_OFFSET
+
+typedef struct {
+    uint8_t reg;
+    uint8_t len;
+    char data[BUFFER_SIZE-FIFO_DATA_OFFSET];
+} i2c_msg_t;
+
+typedef struct {
+    uint8_t head;
+    uint8_t tail;
+    uint8_t has_data;
+    uint8_t idx;
+} fifo_status_t;
+
+fifo_status_t _fs = {
+    .head = 0,
+    .tail = 0,
+    .has_data = 0,
+    .idx = 0
+};
+
+i2c_msg_t _fifo[FIFO_SIZE];
 
 void i2c_receive_handler(uint8_t data, bool is_address)
 {
-    if ((_head == _tail) && _has_data) {
+    static bool previous_was_address = false;
+    if ((_fs.head == _fs.tail) && _fs.has_data) {
         //Buffer is full, do nothing.
         return;
     }
     if (is_address) {
-        _cur_op = data;
-        printf("\nAddress: %X, receiving...", data);
-    }
-    else {
-        if ((_cur_op == OSTENTUS_CMD_ADDR) && (_cmd_idx == 0)) {
-            _fifo[_tail][_cmd_idx++] = data;
-        } else if (_cur_op == OSTENTUS_DAT_ADDR) {
-            if (_dat_idx < BUFFER_SIZE) {
-                _fifo[_tail][_dat_idx] = data;
-                ++_dat_idx;
+        previous_was_address = true;
+    } else {
+        if (previous_was_address) {
+            _fifo[_fs.tail].reg = data;
+            previous_was_address = false;
+        } else {
+            _fifo[_fs.tail].data[_fs.idx] = data;
+            ++_fs.idx;
+            if (_fs.idx >= FIFO_DATA_SIZE) {
+                --_fs.idx;
             }
         }
     }
@@ -50,54 +69,45 @@ void i2c_request_handler(uint8_t address)
 
     switch (address)
     {
-    case 0x70:
+    case OSTENTUS_ADDR:
         buffer[0] = 0x10;
         buffer[1] = 0x11;
         buffer[2] = 0x12;
-        break;
-    case 0x71:
-        sprintf((char *)buffer, "Hello, I'm %X", address);
         break;
     }
 }
 
 void i2c_stop_handler(uint8_t length)
 {
-    if (_cur_op != OSTENTUS_CMD_ADDR) {
-        return;
-    }
     //Write data len
-    _fifo[_tail][FIFO_DATA_OFFSET-1] = _dat_idx-FIFO_DATA_OFFSET;
+    _fifo[_fs.tail].len = _fs.idx;
     //Indicate there's info in the fifo
-    ++_has_data;
-    if (++_tail >= FIFO_SIZE) {
-        _tail = 0;
+    ++_fs.has_data;
+    if (++_fs.tail >= FIFO_SIZE) {
+        _fs.tail = 0;
     }
 
     //Reset to defaults
-    if (_head != _tail) {
-        memset(_fifo[_tail], 0, BUFFER_SIZE);
-    }
-    _cmd_idx = 0;
-    _dat_idx = FIFO_DATA_OFFSET;
-    _cur_op = 0;
+    _fs.idx = 0;
 }
 
 int fifo_pop(uint8_t * r_buf) {
-    if (_has_data == 0) {
+    if (_fs.has_data == 0) {
         return -1;
     }
-    memcpy(r_buf, _fifo[_head], BUFFER_SIZE);
-    ++_head;
-    if (_head >= FIFO_SIZE) {
-        _head = 0;
+    r_buf[0] = _fifo[_fs.head].reg;
+    r_buf[1] = _fifo[_fs.head].len;
+    memcpy(r_buf+2, _fifo[_fs.head].data, _fifo[_fs.head].len);
+    ++_fs.head;
+    if (_fs.head >= FIFO_SIZE) {
+        _fs.head = 0;
     }
-    --_has_data;
+    --_fs.has_data;
     return 0;
 }
 
 bool fifo_has_data(void) {
-    if (_has_data) {
+    if (_fs.has_data) {
         return true;
     }
     return false;
@@ -105,17 +115,13 @@ bool fifo_has_data(void) {
 
 void fifo_init(void)
 {
-    _cur_op = 0;
-    _has_data = 0;
-    _cmd_idx = 0;
-    _dat_idx = 0;
-    _head = 0;
-    _tail = 0;
-    memset(_fifo[_head], 0, BUFFER_SIZE);
+    _fs.head = 0;
+    _fs.tail = 0;
+    _fs.has_data = 0;
+    _fs.idx = 0;
 
     i2c_multi_init(pio, pin);
-    i2c_multi_enable_address(OSTENTUS_CMD_ADDR);
-    i2c_multi_enable_address(OSTENTUS_DAT_ADDR);
+    i2c_multi_enable_address(OSTENTUS_ADDR);
     i2c_multi_set_receive_handler(i2c_receive_handler);
     i2c_multi_set_request_handler(i2c_request_handler);
     i2c_multi_set_stop_handler(i2c_stop_handler);
