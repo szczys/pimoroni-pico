3c3c5d813d01c75f9f91afad0b35f6cca6ed1630
Mike Szczys <mike@golioth.io>
2023-02-05 16:30:38 -0600

    remove custom patches
    Custom patches for micropython will be applied from the Ostentus repo
    and do not need to be included in this fork.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- micropython/_board/badger2040/fixup.sh
+++ micropython/_board/badger2040/fixup.sh
@@ -1,10 +1,6 @@
 SRC_DIR=$1
 DST_DIR=$2
 
-echo "Applying rp2_pio_c.patch"
-cd "$DST_DIR"
-git apply "$SRC_DIR/rp2_pio_c.patch"
-
 echo "Applying wakeup_gpio.patch"
 cd "$DST_DIR/../../lib/pico-sdk"
 git apply "$SRC_DIR/wakeup_gpio.patch"
